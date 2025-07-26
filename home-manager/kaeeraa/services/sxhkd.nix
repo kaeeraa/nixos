0741445e93c30940bd16ce0206588aa4a5791ea1
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-07-26 19:21:20 +0300

    fix(home-manager/kaeeraa): fix binds usage

--- home-manager/kaeeraa/services/sxhkd.nix
+++ home-manager/kaeeraa/services/sxhkd.nix
@@ -7,9 +7,9 @@
       "super + q" = "bspc node --close";
       "super + alt + r" = "bspc wm -r";
       "super + shift + r" = "pkill -usr1 -x sxhkd; notify-send 'sxhkd' 'Reloaded config'";
-      "super + {1-9,0}" = "bspc desktop -s {1-9,0} --follow";
+      "super + {1-9,0}" = "bspc desktop -f {1-9,10}";
       "super + ctrl + shift + {Left,Right}" = "bspc desktop -m {next,prev} --follow";
-      "super + shift + {1-9,0}" = "bspc node -d '^{1-9,10}' --follow";
+      "super + shift + {1-9,0}" = "bspc node -d '{1-9,10}' --follow";
       "super + shift + {Left,Down,Up,Right}" = "bspc node -z {left -20 0,bottom 0 20,top 0 -20,right 20 0}";
       "super + alt + {Left,Down,Up,Right}" = "bspc node -v {-20 0,0 20,0 -20,20 0}";
       "super + v" = "bspc node --state ~floating";
