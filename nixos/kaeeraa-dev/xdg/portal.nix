5e9982bffc9a5b1fa23da84c7d20dea1c7358fa7
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-07-26 20:56:13 +0300

    fix(xdg): specify default portal

--- nixos/kaeeraa-dev/xdg/portal.nix
+++ nixos/kaeeraa-dev/xdg/portal.nix
@@ -1,6 +1,11 @@
 { pkgs, ... }: {
   xdg.portal = {
     enable = true;
+    config = {
+      common.default = [
+        "gtk"
+      ];
+    };
     extraPortals = with pkgs; [
       xdg-desktop-portal-gtk
       kdePackages.xdg-desktop-portal-kde
