ffffdadb405ffc592d837e698fafcdf5dfe2aabf
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-07-28 15:48:50 +0300

    fix(environment): make a workaround for nh. Ref: nix-community/nh#236

--- nixos/kaeeraa-dev/environment/default.nix
+++ nixos/kaeeraa-dev/environment/default.nix
@@ -8,6 +8,8 @@
         background = ../assets/nixos-wallpaper.png;
         loginBackground = true;
       })
+      (pkgs.writeShellScriptBin "sudo" "doas $@")
+      (pkgs.writeShellScriptBin "run0" "doas $@")
     ];
   };
 }
