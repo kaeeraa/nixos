4b53eb1b37df6475788dc3bcb5f9ba8494bb0c9d
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-08-01 10:00:20 +0300

    feat: enable xsession only if hostname matches

--- home-manager/kaeeraa/default.nix
+++ home-manager/kaeeraa/default.nix
@@ -1,13 +1,20 @@
 {
-  imports = [
-    ./colors.nix
-    ./home
-    ./programs
-    ./services
-    ./xsession
-    ./gtk.nix
-    ./dconf.nix
-    ./xdg.nix
-    ./xresources.nix
-  ];
+  config,
+  lib,
+  ...
+}: {
+  imports =
+    [
+      ./colors.nix
+      ./home
+      ./programs
+      ./services
+      ./gtk.nix
+      ./dconf.nix
+      ./xdg.nix
+      ./xresources.nix
+    ]
+    ++ lib.mkIf (config.networking.hostname == "kaeeraa-dev") [
+      ./xsession
+    ];
 }
