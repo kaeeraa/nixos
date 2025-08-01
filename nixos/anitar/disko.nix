2f4afd793605c6eb646048664ce349e93996ad01
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-08-01 12:38:20 +0300

    feat(disko): change device

--- nixos/anitar/disko.nix
+++ nixos/anitar/disko.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/disk/by-id/ata-WALRAM_512GB_AA000000000000002946";
+        device = "/dev/disk/by-id/ata-QEMU_DVD-ROM_QM00003";
         content = {
           type = "gpt";
           partitions = {
