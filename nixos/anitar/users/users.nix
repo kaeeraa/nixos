0500e830caee37daf977676033b02e8bd130819e
Kaeeraa <kaeeraa@nebula-nook.ru>
2025-09-14 21:35:19 +0300

    fix(anitar): give password for users

--- nixos/anitar/users/users.nix
+++ nixos/anitar/users/users.nix
@@ -8,6 +8,7 @@
       enable = true;
       isNormalUser = true;
       extraGroups = ["wheel" "networkmanager" "doas"];
+      hashedPassword = "$y$j9T$KnF5DX.ZKlSYfgoMyCjYR0$NISm9zSUgWBLzIEFKVZrNJsZBE/RhiGlVjmMt8fBJU3";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH40U4M9IIt9CjHtjQxkWXHhp6dQ3hHvGrOYca7RM7EB kaeeraa@kaeeraa-dev"
       ];
@@ -16,10 +17,12 @@
       enable = true;
       isNormalUser = true;
       extraGroups = ["wheel" "networkmanager" "doas"];
+      hashedPassword = "$y$j9T$MDV8AS5BT3.sW9L4PddUW.$zMS0.cbH45fG0GlporXf9He0ubGTTfQzt8ilO0clAb3";
       openssh.authorizedKeys.keys = [
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAG9vLzRoIqyiFTZ7pboRvMh822TMK5Lq64w+AUBI9+Y notwindstone@gmail.com"
       ];
     };
+    root.hashedPassword = "$y$j9T$c/9C6B3uB1Q0uoSaE4qLD/$0m1OIPyUPqPu8nZrJrCwui1LANU1t.k2OMrZyqSYqlA";
     root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOiS+V5QMGDfHlKtBQzsIfvQA1Nb+sRpBH6mlFJepIpH kaeeraa@kaeeraa-dev"
     ];
